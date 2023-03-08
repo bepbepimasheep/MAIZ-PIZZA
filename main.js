6229f266186712132fb104c0001c19333c81d094
bepbepimasheep <113216082+bepbepimasheep@users.noreply.github.com>
2023-03-08 19:53:47 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+menuListArray = ["pizza vegetariana",
+    "pizza de frango",
+    "pizza portuguesa",
+    "pizza quatro queijos",
+    "pizza de calabresa",
+    "pizza extravaganza"];
+
+
+function getMenu() {
+    var htmldata="";
+    menuListArray.sort();
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+ menuListArray[i] + '<br>';
+    }
+document.getElementById("displayMenu").innerHTML = htmldata;
+} 
+
+function addItem(){
+    var htmldata;
+    var imgtags='<img id="im1" src="pizzaImg.png"/>'
+    var item=document.getElementById("addItem").value;
+    menuListArray.sort();
+    htmldata=""
+    for(var i=0;i<menuListArray.length;i++){
+        htmldata=htmldata+imgtags+ menuListArray[i]+'<br>';
+    }
+     document.getElementById("displayAddedMenu").innerHTML = htmldata;
+     
+}
+
+
+//crie a função addTop
+function addTop() {
+    var item=document.getElementById("addItem").value;
+    menuListArray.push(item);
+    addItem();
+}
